f19c85ae301e5e920c95a314f9b80b54e3789bc1
taeong <ty990520@naver.com>
2021-07-18 14:09:18 +0900

    create CRUD test files & add include tags

---
+++ src/main/java/com/hanium/service/DeptServiceImpl.java
@@ -0,0 +1,44 @@
+package com.hanium.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.hanium.domain.DeptVO;
+import com.hanium.mapper.DeptMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Service
+@AllArgsConstructor
+public class DeptServiceImpl implements DeptService{
+
+	private DeptMapper mapper;
+	
+	@Override
+	public void register(DeptVO dept) {
+		log.info("[SERVICE]register..."+dept);
+		mapper.insert(dept);
+	}
+
+	@Override
+	public DeptVO get(String deptcode) {
+		log.info("[SERVICE]get..."+deptcode);
+		return mapper.read(deptcode);
+	}
+
+	@Override
+	public boolean modify(DeptVO dept) {
+		log.info("[SERVICE]modify..."+dept);
+		return mapper.update(dept)==1;	//update()의 반환형은 int이기 때문에!
+	}
+
+	@Override
+	public List<DeptVO> getList() {
+		log.info("[SERVICE]getList...");
+		return mapper.getList();
+	}
+
+}
---
+++ src/test/java/com/hanium/mapper/DeptMapperTests.java
@@ -0,0 +1,58 @@
+package com.hanium.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.hanium.domain.DeptVO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class DeptMapperTests {
+	@Setter(onMethod_ = @Autowired)
+	private DeptMapper mapper;
+
+	@Test
+	public void testGetList() {
+		mapper.getList().forEach(dept -> log.info(dept.getDeptcode()));
+	}
+	
+	@Test
+	public void testRead() {
+	    DeptVO dept = mapper.read("AA01");	// 원하는 bno값을 입력함, 임의로 bno = 5라고 가정
+	    log.info(dept.getDeptptype() +" "+ dept.getDeptname());
+	}
+	
+	@Test
+	public void testInsert() {
+		DeptVO dept = new DeptVO();
+		dept.setDeptcode("AA02");
+		dept.setDeptname("사이버보안 담당 그룹");
+		dept.setDeptptype("원자력");
+
+	    mapper.insert(dept);
+
+	    log.info(dept.getDeptcode()+":"+dept.getDeptptype()+":"+dept.getDeptname());
+	}
+	
+	@Test
+	public void testUpdate() {
+		DeptVO dept = new DeptVO();
+
+	    // 실행 전 존재하는 번호인지 확인할 것!
+		dept.setDeptcode("AA02");
+		dept.setDeptname("사이버보안 담당 그룹");
+		dept.setDeptptype("원자력");
+
+	    log.info("UPDATE COUNT : "+mapper.update(dept));
+	    //정상적으로 update되었으면 1을 반환함
+	}
+}
