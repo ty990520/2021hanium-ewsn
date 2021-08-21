b49eaa5d01bf031a05dab909edb16df30fc42339
taeong <ty990520@naver.com>
2021-08-21 11:29:47 +0900

    add session login,logout

--- src/main/java/com/hanium/controller/UserController.java
+++ src/main/java/com/hanium/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.hanium.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,19 +44,19 @@ public class UserController {
 
 	@GetMapping("/list")
 	public void list(Criteria cri, Model model) {
-		model.addAttribute("pageMaker", new PageDTO(cri,service.count(cri)));
+		model.addAttribute("pageMaker", new PageDTO(cri, service.count(cri)));
 		log.info(service.count(cri));
 		model.addAttribute("list", service.getList(cri));
 	}
 
 	@GetMapping("/user_request")
-	public void userRequestlist(Criteria cri,Model model) {
+	public void userRequestlist(Criteria cri, Model model) {
 		model.addAttribute("cri", cri);
 		model.addAttribute("userRequest", service.getUserRequest(cri));
 	}
 
 	@GetMapping("/admin_request")
-	public void adminRequestlist(Criteria cri,Model model) {
+	public void adminRequestlist(Criteria cri, Model model) {
 		model.addAttribute("cri", cri);
 		model.addAttribute("adminRequest", service.getAdminRequest(cri));
 	}
@@ -98,16 +101,16 @@ public class UserController {
 		userid = userid.toString();
 		UserVO user = service.findNotOnlyUser(userid);
 
-		//log.info(service.findUserValidity(userid));
+		// log.info(service.findUserValidity(userid));
 		// 1. 사번 유효성 검증
 		if (service.findUserValidity(userid)) {
-			//log.info("[ CONTROLLER ] 사번이 유효합니다.");
+			// log.info("[ CONTROLLER ] 사번이 유효합니다.");
 			// 2. user valid check
 			service.setUserValid(user);
 			return "success";
 
 		} else {
-			//log.info("[ CONTROLLER ] 사번이 유효하지 않음");
+			// log.info("[ CONTROLLER ] 사번이 유효하지 않음");
 			return "fail";
 		}
 	}
@@ -126,7 +129,8 @@ public class UserController {
 
 	@ResponseBody
 	@RequestMapping(value = "/user_login", produces = "application/text; charset=UTF-8")
-	public String login(@RequestParam("userid") String userid, @RequestParam("userpw") String userpw) {
+	public String login(HttpServletRequest request, @RequestParam("userid") String userid,
+			@RequestParam("userpw") String userpw) throws Exception {
 		log.info(userid + "," + userpw);
 		UserVO user = service.get(userid);
 		log.info(user);
@@ -137,6 +141,16 @@ public class UserController {
 			if (service.login(userid, userpw)) {
 				String username = user.getUsername();
 				log.info("[ CONTROLLER ] 로그인 성공 : " + username);
+				HttpSession session = request.getSession();
+				String login_name = username;
+				String login_id = user.getUserid();
+				String login_dept = user.getUserdept();
+				String login_ptype = user.getUserptype();
+				session.setAttribute("login_name", login_name);
+				session.setAttribute("login_id", login_id);
+				session.setAttribute("login_dept", login_dept);
+				session.setAttribute("login_ptype", login_ptype);
+
 				return username + "님 환영합니다!";
 
 			} else {
@@ -144,5 +158,12 @@ public class UserController {
 				return "로그인에 실패하였습니다.";
 			}
 		}
+	}
+
+	@RequestMapping("/logout")
+	public String logout(HttpServletRequest request) throws Exception {
+		HttpSession session = request.getSession();
+		session.invalidate();
+		return "redirect:/";
 	}
 }
