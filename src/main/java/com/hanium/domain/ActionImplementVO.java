7b97025892afe594a27aeeb877a41782e4674fd0
hyezzingzzing2 <hihello1208@naver.com>
2021-10-03 13:58:27 +0900

    ActionImplement Modify

--- src/main/java/com/hanium/domain/ActionImplementVO.java
+++ src/main/java/com/hanium/domain/ActionImplementVO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class ActionImplementVO {
 	private Long AI_no; //권고발행번호
-	private Boolean AI_check ; //이행여부 
+	private char AI_check ; //이행여부 
 	private String AI_impossibleReason; // 조치불가사유
 	private String AI_AlterMeasures;  //대안조치
 	private String AI_result; //이행결과 
